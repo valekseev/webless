5f70bc3de2550fe8b6bfa7015fb0effb4f3a5745
valekseev <valekseev@gmail.com>
2013-11-16 17:12:36 +0400

    Log size limit

--- src/main/java/org/al/ServicesInit.java
+++ src/main/java/org/al/ServicesInit.java
@@ -4,19 +4,24 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @WebListener
 public class ServicesInit implements ServletContextListener {
+    private static final Logger logger = LoggerFactory.getLogger(ServletContextListener.class);
+
 
 	@Override
 	public void contextInitialized(ServletContextEvent event) {
-        System.out.println("Starting spamming");
+	    logger.info("Starting spammer");
 		SpammerService.getInstance().start();
     }
 	
 	@Override
     public void contextDestroyed(ServletContextEvent event) {
+	    logger.info("Stopping spammer");
     	SpammerService.getInstance().shutdown();
-        System.out.println("Stopped spamming");
     }
 
 }
