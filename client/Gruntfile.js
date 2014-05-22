4c132f0cfcdc966180508f87084ec257207a64ce
valekseev <valekseev@gmail.com>
2014-05-23 02:46:24 +0400

    Added throttling proxy task

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -3,19 +3,31 @@
 module.exports = function (grunt) {
 
   grunt.initConfig({
+    //Simple web server supporting Range headers
     connect: {
       server: {
         options: {
-          port: 8080,
-          keepalive: true
+          port: 8081
         }
+      }
+    }, 
+    //HTTP proxy adding bandwidth limits and latency delays simulation
+    throttle: {
+      default: {
+        remote_port: 8081,
+        local_port: 8080,
+        upstream: 256*1024,
+        downstream: 512*1024,
+        keepalive: true,
+        latency: 300
       }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-connect');
 
-  // Default task(s).
-  grunt.registerTask('default', ['connect']);
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-throttle');
+
+  grunt.registerTask('default', ['connect','throttle']);
   
 };
